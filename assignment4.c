06123f2d14f6f42b6a7b868f7a016aeefd81b7cf
99002486 <71490592+99002486@users.noreply.github.com>
2020-10-17 10:00:58 +0530

    Add files via upload

--- assignment4.c
+++ assignment4.c
@@ -1,14 +1,16 @@
+/* mini shell that can take 6 arguments*/
 #include<stdio.h>
 #include<unistd.h>
 #include<string.h>
+#include<sys/wait.h>
 
 int main()
 {
     pid_t ret;
     int status;
     char CLIin[100];
-    printf("Enter your command: ");
-    scanf("%[^\n]%*c", CLIin);
+    printf("Enter your command:");
+    scanf("%[^\n]%*c", CLIin);                  //read line
     ret=fork();
     //printf("%s\n",CLIin);
     if(ret<0)
@@ -20,23 +22,23 @@ int main()
     {
         
         int cmd_exec,cmd_flag=0,arg_index=0;
-        char *cmd,*argmts[10]={0};
+        char *argmts[6]={};                     //initilaize null array
+        /*splitting input and storing into array as arguments*/
         char* token;
         char sep[2] = " ";
         token = strtok(CLIin," ");
         while (token != NULL)
         {
             argmts[arg_index]=token;
-            printf("%s\n",argmts[arg_index]);
+            printf("%s %d\n",argmts[arg_index],arg_index);
             token = strtok(NULL, " ");
             arg_index++;
         }
-        argmts[arg_index] = NULL;
-        cmd=argmts[0];
-        cmd_exec=execv(strcat("usr/bin",cmd),argmts);
+        /*executing using execp*/
+        cmd_exec=execlp(argmts[0],argmts[0], argmts[1],argmts[2],argmts[3],argmts[4],argmts[5]);
         if(cmd_exec<0)
         {
-            perror("execv");
+            perror("execlp");
             exit(2);
         }
         exit(0);
@@ -44,6 +46,9 @@ int main()
     else
     {
         waitpid(-1,&status,0); //wait(&status);
+        printf("parent--child exit status=%d\n",
+			WEXITSTATUS(status));
     }
+
     
 }
