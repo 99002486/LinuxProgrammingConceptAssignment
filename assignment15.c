6931ee62524863a26099b1f05c68f42758e9d21e
99002486 <71490592+99002486@users.noreply.github.com>
2020-10-18 16:24:59 +0530

    Add files via upload

--- assignment15.c
+++ assignment15.c
@@ -1,54 +1,39 @@
-/* mini shell that can take 6 arguments*/
-#include<stdio.h>
 #include<unistd.h>
-#include<string.h>
-#include<sys/wait.h>
+#include<fcntl.h>
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 int main()
 {
-    pid_t ret;
-    int status;
-    char CLIin[100];
-    printf("Enter your command:");
-    scanf("%[^\n]%*c", CLIin);                  //read line
-    ret=fork();
-    //printf("%s\n",CLIin);
-    if(ret<0)
+    int fd1,size_buf;
+	fd1 =open("random.txt",O_RDONLY);
+	if(fd1<0)
+	{
+		perror("open");
+		exit(1);
+	}
+	int maxlen=300;
+	char buf[maxlen];
+    int fd2,size_file;
+	fd2=open("random1.txt",O_WRONLY|O_CREAT, 0666);
+	if(fd2<0)
+	{
+		perror("open");
+		exit(3);
+	}
+    while(size_buf=read(fd1,buf,maxlen))
     {
-        perror("fork");
-        exit(1);
-    }
-    if(ret==0)
-    {
-        
-        int cmd_exec,cmd_flag=0,arg_index=0;
-        char *argmts[6]={};                     //initilaize null array
-        /*splitting input and storing into array as arguments*/
-        char* token;
-        char sep[2] = " ";
-        token = strtok(CLIin," ");
-        while (token != NULL)
-        {
-            argmts[arg_index]=token;
-            printf("%s %d\n",argmts[arg_index],arg_index);
-            token = strtok(NULL, " ");
-            arg_index++;
-        }
-        /*executing using execp*/
-        cmd_exec=execlp(argmts[0],argmts[0], argmts[1],argmts[2],argmts[3],argmts[4],argmts[5]);
-        if(cmd_exec<0)
-        {
-            perror("execlp");
-            exit(2);
-        }
-        exit(0);
-    }
-    else
-    {
-        waitpid(-1,&status,0); //wait(&status);
-        printf("parent--child exit status=%d\n",
-			WEXITSTATUS(status));
+	    size_file =	write(fd2, buf, maxlen);
+        if(size_file<0)
+	    {
+		    perror("write");
+		    exit(4);
+	    }
     }
 
     
+    close(fd1);
+    close(fd2);
+    printf("Copy Successful \n");
 }
